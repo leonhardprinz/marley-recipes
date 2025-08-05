bb3c26a8b44a8f2606440f1246d342c36af97b98
leonhardprinz <leonhardprinz@gmail.com>
2025-08-05 10:22:36 -0700

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: 'Marley Spoon Rezepte',
+  description: 'Public React app for highlighting Marley Spoon recipes with search and filters.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body style={{ margin: 0, padding: 0, fontFamily: "'Poppins', sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
