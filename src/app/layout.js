9a227c5236557ecc43a36292c19adb03050ceadc
leonhardprinz <leonhardprinz@gmail.com>
2025-08-05 09:53:31 -0700

    Add Google Inter font to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body style={{ margin: 0, padding: 0, fontFamily: "'Inter', sans-serif" }}>
+        {children}
+      </body>
     </html>
   );
 }
